c1f8fc16d7c3af5f78e915d3b99862b8275bb980
bks <bks@cs.rit.edu>
2023-01-12 14:29:43 -0500

    added main(void) skeleton code

---
+++ main.c
@@ -0,0 +1,17 @@
+//
+// TODO file documentation
+// author: TODO
+// description:
+// TODO
+//
+
+#include <stdio.h>
+
+/// TODO main function documentation
+/// @return status value for OS use
+
+int main( void) {
+    
+    return 0;
+}
+
